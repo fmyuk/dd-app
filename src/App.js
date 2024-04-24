7c95372b7831de6bbafbd0dac6b759d41bf34066
fumi uk <dcfumiya.238@gmail.com>
2024-04-25 08:16:25 +0900

    dd-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1 style={{ margin: "20px" }}>Trello風アプリ</h1>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
